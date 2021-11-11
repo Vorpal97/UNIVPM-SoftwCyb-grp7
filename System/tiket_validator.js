85183e81bf38f6a0adefd8e34ab1adc88f399c78
Manuel Manelli <manuelmanelli@icloud.com>
2021-11-11 18:50:45 +0100

    Implementata la vidimazione dei biglietti

--- System/tiket_validator.js
+++ System/tiket_validator.js
@@ -1,6 +1,15 @@
 const Web3 = require("web3");
 
-let nodo = 'http://localhost:22002';
+var express = require('express');
+var bodyParser = require('body-parser');
+var serveStatic = require('serve-static');
+var app     = express();
+const path = require('path');
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+let nodo = 'http://localhost:22003';
 
 let web3 = new Web3(nodo);
 
@@ -12,58 +21,213 @@ var abi = cont.abi;
 
 web3.eth.getAccounts().then((value)=> {
   console.log("Accounts: " + value);
-  demo(value[0]);
+  main(value[0]);
 });
 
 function sleep(ms){
   return new Promise(resolve =>setTimeout(resolve, ms));
 }
+async function getEventData(address){
 
-
-function demo(a){
-  console.log("used account: " + a);
-
-  var address = "0x84272f3E148eb3f957E3Df666d91a15a6Bd1DB7F";
-
-
-  //var Contract = new web3.eth.contract.setProvider()
   var Contract = require('web3-eth-contract');
-
-  // set provider for all later instances to use
   Contract.setProvider(nodo);
-
   var contract = new Contract(abi, address);
+  tmp = {};
 
-      var idBiglietto= 3;
-      console.log(a);
-      contract.methods.vidimaBiglietto(idBiglietto).send({from:a})
-      .on('receipt', function(){
-        contract.methods.isBigliettoVidimato(idBiglietto).call()
-        .then((endorsed) => {
-          if(endorsed){
-            console.log("Brav uagliù");
-            contract.methods.getBiglietto(idBiglietto).call()
-            .then((biglietto) => {
-              v = Object.values(biglietto)
-                console.log("getBiglietto(): \n" +
-                            "ticketId = " + v[1] + "\n" +
-                             "validateHash = " + v[2] + "\n" +
-                             "isValid = " + v[3] + "\n" +
-                             "endorsed = " + v[4] + "\n" +
-                             "ownerAddress = " + v[5] + "\nFINE")
+  await contract.methods.getLuogo().call()
+    .then((result) => {
+      tmp.luogo = result;
+    })
+
+  await contract.methods.getNomeEvento().call()
+  .then((result) => {
+    tmp.nome = result;
+  });
+
+  await contract.methods.getDataEvento().call()
+  .then((result) => {
+    tmp.data = result;
+  });
+
+  await contract.methods.getNumPosti().call()
+  .then((result) => {
+    tmp.numPosti = result;
+  });
+
+  await contract.methods.getPostiDisponibili().call()
+  .then((result) => {
+    tmp.postiDisponibili = result;
+  });
+
+  await contract.methods.getPrezzoBiglietto().call()
+  .then((result) => {
+    tmp.prezzoBiglietto = result;
+  });
+
+  await contract.methods.getStato().call()
+  .then((result) => {
+    switch(result){
+      case '0':
+        tmp.stato = "inVendita";
+        break;
+      case '1':
+        tmp.stato = "inCorso";
+        break;
+      case '2':
+        tmp.stato = "sospeso";
+        break;
+      case '3':
+        tmp.stato = "soldout";
+        break;
+      case '4':
+        tmp.stato = "cancellato";
+        break;
+      case '5':
+        tmp.stato="terminato";
+        break;
+      default:
+      tmp.stato="errore";
+        break;
+    }
+  });
+
+  await contract.methods.getTicketCounter().call()
+  .then((result) => {
+    tmp.ticketCounter = result;
+  });
+
+  return JSON.stringify(tmp);
+}
+
+
+function main(a){
+
+  app.listen(8080, function() {
+    console.log('Il client del Ticket Validator è accessibile a http://127.0.0.1:8080/');
+  });
+
+  app.get('/', (req, res) => {
+      res.sendFile(path.resolve('../Client/ticket_inspector/index.html'));
+  });
+
+  app.get('/style', (req, res) => {
+      res.sendFile(path.resolve('../Client/css/main.css'));
+  });
+
+  app.get('/visualizzaeventi', (req, res) => {
+      res.sendFile(path.resolve('../Client/ticket_inspector/VisualizzaEventi.html'));
+  });
+
+  app.get('/visualizzabiglietti', (req, res) => {
+        var address = req.query.address;
+        if(address == null)
+          res.end("");
+
+        fs = require('fs');
+        var data = fs.readFileSync('../Client/ticket_inspector/VisualizzaBiglietti.html', 'utf8');
+        data = data.replace("${address}", address);
+
+       res.setHeader('Content-Type', 'text/html');
+       res.end(data);
+  });
+
+  app.get('/address', (req, res) => {
+      res.setHeader('Content-Type', 'application/json');
+      res.end(JSON.stringify({ address: a }));
+  });
+
+  app.get('/eventi', async(req, res) => {
+      var listaEventi = [];
+      fs = require('fs');
+      var data = fs.readFileSync('contracts.json', 'utf8');
+      listaEventi = data.split("\n")
+      var o = [];
+      for(i = 0; i < listaEventi.length - 1; i++){
+        var address = JSON.parse(listaEventi[i]).address
+        var a = {};
+        await getEventData(address).then((result) => {
+            a = result;
+        })
+        a = JSON.parse(a)
+        a.address = address;
+        if(a.stato == "inCorso"){
+          o.push(a);
+        }
+      }
+      res.setHeader('Content-Type', 'application/json');
+      res.end(JSON.stringify(o));
+  });
+
+  app.get('/alltickets', async(req, res) => {
+      var Contract = require('web3-eth-contract');
+      Contract.setProvider(nodo);
+      var address = req.query.address;
+      console.log(address)
+      var contract = new Contract(abi, address);
+      var ticketcounter;
+      var list = [];
+      await contract.methods.getTicketCounter().call()
+            .then((result) => {
+              ticketcounter = result;
             }).catch(()=>{
-              console.log("getBiglietto() errore");
+              console.log("errore sulla getTicketCounter")
             });
-          } else {
-            //Biglietto non vidimato o ticketID non valido o ticket non valido
-            console.log("Errore generico, controlla i dati inseriti.");
-          }
-        }).catch(()=>{
-          console.log("isBigliettoVidimato() errore");
-        });
-     }).catch(()=>{
-       console.log("vidimaBiglietto() errore");
-     });
 
+            for(var j = 0; j < ticketcounter; j++){
+              var o = {};
+              await contract.methods.getBiglietto(j).call()
+                .then((result) => {
+                  obj = Object.values(result);
+                  o.okOp = obj[0];
+                  o.id = obj[1];
+                  o.hash = obj[2];
+                  o.isValid = obj[3];
+                  o.isEndorsed = obj[4];
+                  o.owner = obj[5];
+              }).catch(()=>{
+                console.log("errore sulla getBiglietto")
+              });
+              list.push(o)
+            }
+    console.log("Ritornata lista di biglietti dell'evento: " + a);
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(list));
+  });
+
+  app.post('/vidimabiglietto', async(req, res) => {
+    var address = req.body.address;
+    var id = req.body.id;
+
+    console.log("#######---------------" + address + "#######---------------"+ id)
+
+
+    console.log("used account: " + a);
+
+    //var Contract = new web3.eth.contract.setProvider()
+    var Contract = require('web3-eth-contract');
+
+    // set provider for all later instances to use
+    Contract.setProvider(nodo);
+
+    var contract = new Contract(abi, address);
+    var result = {stato:"errore"}
+
+    await contract.methods.vidimaBiglietto(id).send({from:a})
+    .on('receipt', function(){
+          result = {stato:"successo"}
+          console.log("vidimaBiglietto() ---> OK");
+
+    }).catch(()=>{
+      console.log("vidimaBiglietto() errore");
+      console.log("vidimaBiglietto() ---> Errore");
+
+      result = {stato:"errore"}
+
+    });
+
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(result));
+
+  });
 
 }
